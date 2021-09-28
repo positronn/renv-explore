19dbcfa51af6c190b4515cfc311d8d78b7b14893
positronn <marcodelossantos@protonmail.com>
2021-09-27 22:24:24 -0500

    Adding a ggplot2 scatterplot to  and adding code dependencies in

--- R/iris.R
+++ R/iris.R
@@ -1,3 +1,11 @@
 # iris.R
+library(magrittr)
 
 iris <- tibble::tibble(iris)
+
+iris <- iris %>%
+    janitor::clean_names()
+
+iris %>%
+    ggplot2::ggplot() +
+    ggplot2::geom_point(ggplot2::aes(x=sepal_length, y=petal_length, color=species))
